3a6181721f664e19f9017cb23d899084da918f2c
Gustav <gust2968@stud.kea.dk>
2018-11-26 13:47:30 +0100

    hej

--- src/Sekretær.java
+++ src/Sekretær.java
@@ -1,2 +1,13 @@
 public class Sekretær {
+    private int Id;
+    private int Pin;
+
+    public void getArrangementListe(){
+        return ArrangementListe;
+    }
+    public void tilføjArrangement(){}
+
+    public void sletArrangement(){}
+
+    public void redigerArrangement(){}
 }
