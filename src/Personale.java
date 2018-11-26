550e05f1e6ee0acd03ce66b80f03b21f6dddd071
Gustav <gust2968@stud.kea.dk>
2018-11-26 13:58:57 +0100

    hej

--- src/Personale.java
+++ src/Personale.java
@@ -1,2 +1,12 @@
 public class Personale {
+    private int Id;
+    private int Pin;
+
+    public boolean ValiderPersonale(int id, int pin) {
+        if (id == Id && pin == Pin) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
